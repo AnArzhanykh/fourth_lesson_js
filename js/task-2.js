bf2cbb14e87048fc0255c8232f86edf3ef12db7a
AnArzhanykh <45142693+AnArzhanykh@users.noreply.github.com>
2021-03-24 13:12:42 +0200

    fix 2 task bind

--- js/task-2.js
+++ js/task-2.js
@@ -1,22 +1,22 @@
 const inventory = {
-   items: ['Knife', 'Gas mask'],
-   add(itemName) {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
       console.log(`Adding ${itemName} to inventory`);
 
-      inventory.items.push(itemName);
-   },
-   remove(itemName) {
+      this.items.push(itemName);
+  },
+  remove(itemName) {
       console.log(`Removing ${itemName} from inventory`);
 
-      inventory.items = inventory.items.filter(item => item !== itemName);
-   },
+      this.items = this.items.filter(item => item !== itemName);
+  },
 };
 
-
 const invokeInventoryAction = function(itemName, action) {
-console.log(`Invoking action on ${itemName}`);
-action(itemName);
-};  
+  console.log(`Invoking action on ${itemName}`);
+  const inventoryAction = action.bind(inventory)
+  inventoryAction(itemName);
+};
 
 
 
@@ -24,9 +24,13 @@ invokeInventoryAction('Medkit', inventory.add);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
+
+
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
 invokeInventoryAction('Gas mask', inventory.remove);
-//  // Invoking action on Gas mask
-//  // Removing Gas mask from inventory
- console.log(inventory.items); // ['Knife', 'Medkit']
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
+
